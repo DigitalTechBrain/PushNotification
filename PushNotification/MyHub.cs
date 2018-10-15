c26c0c2a8be9b19eec32a769f2788bc0e43734f2
Jitender <user@USER-PC>
2018-10-15 18:51:23 +0530

    Push Notification Using SignalR

---
+++ PushNotification/MyHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PushNotification
+{
+    public class MyHub : Hub
+    {
+
+        public static void Show()
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+            context.Clients.All.displayStatus();
+        }
+
+    }
+}
---
+++ PushNotification/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace PushNotification
+{
+    public class Products
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public decimal PricDecimal { get; set; }
+        public decimal QuantDecimal { get; set; }
+    }
+}
